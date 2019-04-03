e571a151f3c9fa3c8c904e447ce3000ccf3f19d6
thanhthu0321 <45757676+thanhthu0321@users.noreply.github.com>
2019-04-03 10:45:05 +0700

    View Score for School
    SQL Server
    MVC2
    .Net C#
    OOP

---
+++ ProjectViewScore/ProjectViewScore/sample.connection/MyConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ProjectViewScore.sample.connection
+{
+    public class MyConnection
+    {
+        public static SqlConnection getMyConnection()
+        {
+            string connection = @"server = localhost; User ID = sa; Password = 123; database=QLSVien";
+            SqlConnection con = new SqlConnection(connection);
+            con.Open();
+            return con;           
+        }
+    }
+}
